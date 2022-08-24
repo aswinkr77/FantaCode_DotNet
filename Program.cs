73472efec548d2192d02a079eef19f6b11458003
Aswin K R <aswinkr77@gmail.com>
2022-08-24 22:01:29 +0530

    Created Helloworld and Month, Day Script

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Hello, World
+Console.WriteLine("Hello, World!");
+
+// Current Day
+Console.WriteLine("The current Day: " + DateTime.Now.ToString("dddd"));
+
+// Current Month
+Console.WriteLine("The current Month: " + DateTime.Now.ToString("MMMM"));
